c7c3a21256497dbcd5e619d96341957035b80d18
Anderson Alves <andersonbarretodev@gmail.com>
2024-01-03 16:45:52 -0300

    Finalizando a Aula de Operadores Aritiméticos

--- Aulas/Calculadora.cs
+++ Aulas/Calculadora.cs
@@ -9,22 +9,55 @@ namespace DecolaTech.Aulas
     {
         public void Somar(int x, int y)
         {
-            Console.WriteLine($"Soma {x} + {y} = {x + y}");
+            Console.WriteLine($"Soma: {x} + {y} = {x + y}");
         }
 
         public void Subtrair(int x, int y)
         {
-            Console.WriteLine($"Subtração {x} - {y} = {x - y}");
+            Console.WriteLine($"Subtração: {x} - {y} = {x - y}");
         }
 
         public void Multiplicar(int x, int y)
         {
-            Console.WriteLine($"Multiplicação {x} x {y} = {x * y}");
+            Console.WriteLine($"Multiplicação: {x} x {y} = {x * y}");
         }
 
         public void Dividir(int x, int y)
         {
-            Console.WriteLine($"Divisão {x} / {y} = {x / y}");
+            Console.WriteLine($"Divisão: {x} / {y} = {x / y}");
+        }
+
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);  //utilizado a classe "Math", onde já tem várias operações prontas.
+            Console.WriteLine($"Potência: {x} ^ {y} = {pot}");
+        }
+
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo}* = {Math.Round(seno, 4)}");
+        }
+
+        public void Cosseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double cosseno = Math.Cos(radiano);
+            Console.WriteLine($"CosSeno de {angulo}* = {Math.Round(cosseno, 4)}");
+        }
+
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo}* = {Math.Round(tangente, 4)}");
+        }
+
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz Quadrada de {x} = {raiz}");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,3 +17,8 @@ calc.Somar(10, 30);
 calc.Subtrair(10, 50);
 calc.Multiplicar(15, 45);
 calc.Dividir(2, 2);
+calc.Potencia(3, 3);
+calc.Seno(30);
+calc.Cosseno(30);
+calc.Tangente(30);
+calc.RaizQuadrada(9);
